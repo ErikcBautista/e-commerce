fcba56530e8baa587ee516f2559f1c3fda5274cf
ErikcBautista <erickfcobauhernandez@gmail.com>
2022-11-06 22:09:10 -0600

    feat: implements logic to delete into file [id].js and return a response of not content

--- src/pages/api/products/[id].js
+++ src/pages/api/products/[id].js
@@ -2,15 +2,41 @@
 import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient();
 const products =  async(req, res) => {
-    const product = await prisma.products.findUnique({
-        where :{
-            idProduct : parseInt(req.query.id)
-        }
-    })
-    if(!product){
-        return res.status(404).json({message:'Product not found'})
+    switch (req.method) {
+        case 'GET':
+            const product = await prisma.products.findUnique({
+                where :{
+                    idProduct : parseInt(req.query.id)
+                }
+            })
+            if(!product){
+                return res.status(404).json({message:'Product not found'})
+            }
+            return res.status(200).json({data:product,message:'Success request'})
+            break;
+        case 'DELETE':
+            prisma.$connect();
+            const productFind = await prisma.products.findUnique({
+                where :{
+                    idProduct : parseInt(req.query.id)
+                }
+            })
+            if(!productFind){
+                return res.status(404).json({message:'Product not found'})
+            }
+            await prisma.products.delete({
+                where : {
+                    idProduct : parseInt(req.query.id)
+                }
+            })
+            prisma.$disconnect()
+            return res.status(204).end();
+        
+        default:
+            break;
     }
-    return res.status(200).json({data:product,message:'Success request'})
+    
+    
 }
 
 export default products;
